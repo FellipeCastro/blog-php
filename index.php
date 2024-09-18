58cb7a24bf9aeb3112fbdaa55d82f14cf3ae3059
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-18 17:59:37 -0300

    Aula 27 finalizada

--- index.php
+++ index.php
@@ -4,16 +4,5 @@
 require_once "configuracao.php"; 
 include_once "helpers.php"; 
 
-echo "R$" . formatarValor(10000);
-echo formatarNumero(1000000000000);
-
-// $valor = 3;
-// if ($valor) {
-//     echo $valor;
-// } else {
-//     echo 0;
-// }
-
-// echo $valor ? $valor : 0;
-// echo $valor ?: 0;
+echo $data = date("d/m/Y H:i:s");
 ?>
