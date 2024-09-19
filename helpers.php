3dc34921a92ed1d5bfcd8fb650d5ccb3d7f30e28
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-19 18:50:43 -0300

    Aula 29 finalizada

--- helpers.php
+++ helpers.php
@@ -1,6 +1,16 @@
-<?php 
+<?php
+function validarUrl(string $url): bool
+{
+    return filter_var($url, FILTER_VALIDATE_URL);
+}
 
-function contarTempo(string $data): string {
+function validarEmail(string $email): bool
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function contarTempo(string $data): string
+{
     $agora = strtotime(date("Y-m-d H:i:s"));
     $tempo = strtotime($data);
     $diferenca = $agora - $tempo;
@@ -30,15 +40,18 @@ function contarTempo(string $data): string {
     }
 };
 
-function formatarValor(float $valor = null): string {
+function formatarValor(float $valor = null): string
+{
     return number_format($valor ? $valor : 0, 2, ",", ".");
 };
 
-function formatarNumero(string $numero = null): string {
+function formatarNumero(string $numero = null): string
+{
     return number_format($numero ? $numero : 0, 0, ".", ".");
 };
 
-function saudacao() {
+function saudacao()
+{
     echo $hora = date("H");
 
     if ($hora >= 0 && $hora <= 5) {
@@ -61,7 +74,8 @@ function saudacao() {
  * @param string $continue opcional - o que deve ser exibido ao final do resumo
  * @return string texto resumido
  */
-function resumirTexto(string $texto, int $limite, $continue = "..."): string {
+function resumirTexto(string $texto, int $limite, $continue = "..."): string
+{
     $textoLimpo = trim(strip_tags($texto));
     if (mb_strlen($textoLimpo) <= $limite) {
         return $textoLimpo;
@@ -69,6 +83,5 @@ function resumirTexto(string $texto, int $limite, $continue = "..."): string {
 
     $resumirTexto = mb_substr($textoLimpo, 0, mb_strrpos(mb_substr($textoLimpo, 0, $limite), ""));
 
-    return $resumirTexto.$continue;
+    return $resumirTexto . $continue;
 };
-?>
--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php 
+<?php
 // Arquivo index responsável pela inicialização do sistema
 
-require_once "configuracao.php"; 
-include_once "helpers.php"; 
+require_once "configuracao.php";
+include_once "helpers.php";
 
-echo contarTempo("2022-04-18 13:24:53");
-?>
+$email = "teste@teste.com";
+
+if (validarEmail($email)) {
+    echo "E-mail válido";
+} else {
+    echo "E-mail inválido";
+}
+
+echo "<br>";
+
+$url = "https://www.youtube.com";
+
+if (validarUrl($url)) {
+    echo "URL válida";
+} else {
+    echo "URL inválida";
+}
