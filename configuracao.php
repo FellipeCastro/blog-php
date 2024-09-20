9167c9f3db751aa8410cc7ff91a9fcdb09901bde
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-20 17:25:08 -0300

    Aula 31 finalizada

--- configuracao.php
+++ configuracao.php
@@ -1,4 +1,15 @@
-<?php 
+<?php
 // Arquivo de configuração do sistema
+
+// Define o fuso horário
 date_default_timezone_set("America/Sao_Paulo");
-?>
+
+// Informações do sistema
+define("SITE_NOME", "UnSet");
+define("SITE_DESCRICAO", "UnSet - Tecnologia em Sistemas");
+
+// URL's do sistema
+define("URL_PRODUCAO", "http://unset.com.br");
+define("URL_DESENVOLVIMENTO", "http://localhost/blog-php");
+
+// const SITE = "UnSet";
--- index.php
+++ index.php
@@ -4,28 +4,4 @@
 require_once "configuracao.php";
 include_once "helpers.php";
 
-$email = "teste@teste.com";
-
-if (validarEmail($email)) {
-    echo "E-mail válido";
-} else {
-    echo "E-mail inválido";
-}
-
-echo "<br>";
-
-$url = "https://www.youtube.com";
-
-if (validarUrl($url)) {
-    echo "URL válida";
-} else {
-    echo "URL inválida";
-}
-
-echo "<br>";
-
-if (validarUrlComFiltro($url)) {
-    echo "URL válidada com filtro";
-} else {
-    echo "URL inválidada pelo filtro";
-}
+echo SITE_NOME;
