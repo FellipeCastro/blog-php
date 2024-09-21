d76049c359c39a10aadd5fe1ee2888b2fa533b6d
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-21 13:38:21 -0300

    Aula 37 finalizada

--- index.php
+++ index.php
@@ -4,4 +4,16 @@
 require_once "configuracao.php";
 include_once "helpers.php";
 
-echo saudacao();
+// $numero = 1;
+
+// while ($numero <= 10) {
+//     echo $numero++;
+// }
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "Tabuada do $i" . "<br>";
+    for ($n = 1; $n <= 10; $n++) {
+        echo $i . " x " . $n . " = " . $i * $n . "<br>";
+    }
+    echo "<hr>";
+}
