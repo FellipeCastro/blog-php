900dfd6f2ce899c8110962fc3e05ae8e0a0dcbdf
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-23 17:42:41 -0300

    Aula 46 finalizada

---
+++ Sistema/Nucleo/Controlador.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Sistema\Nucleo;
+
+class Controlador 
+{
+    public function __construct(string $tema = null)
+    {
+        echo $tema;
+    }
+}
--- index.php
+++ index.php
@@ -4,15 +4,10 @@
 require_once "Sistema/configuracao.php";
 include_once "Sistema/Nucleo/Helpers.php";
 include 'Sistema/Nucleo/Mensagem.php';
+include 'Sistema/Nucleo/Controlador.php';
 
-use Sistema\Nucleo\Helpers;
+use Sistema\Nucleo\Controlador;
 
-// use Sistema\Nucleo\Mensagem as Msg;
-// echo (new Msg())->alerta("Mensagem de alerta");
-
-// $helper = new Helpers();
-// echo $helper->validarCpf("44052813863");
-
-// echo Helpers::limparNumero("44052 841-3'863");
-
-echo Helpers::saudacao();
+$controlador = new Controlador();
+echo "<br>";
+var_dump($controlador);
